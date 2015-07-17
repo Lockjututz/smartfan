b27b9c67b822be897a60685ada8bb58a54e989ce
Decker108 <1337decker@gmail.com>
2015-07-17 18:53:52 +0200

    Refactored dbtest.py

--- dbtest.py
+++ dbtest.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 
-import sqlite3
+from __future__ import with_statement
+from pysqlite2 import dbapi2 as sqlite3
 
-conn = sqlite3.connect('/home/pi/smartfan/smartfandb')
-c = conn.cursor()
-c.execute('insert into t_sensor_logs(temperature,humidity) values(1337,108)')
-conn.commit()
-c.close()
+with sqlite3.connect("/home/pi/smartfan/smartfandb") as con:
+    con.execute("insert into t_sensor_logs(temperature,humidity) values(1337,108)")
