dafa4b6075aa680df39fd2ae765da0768c2ad0cd
Decker108 <1337decker@gmail.com>
2015-07-28 20:53:51 +0200

    sensor_repository now works, reverted accidental change to dht11_sensor

--- sensor_repository.py
+++ sensor_repository.py
@@ -5,16 +5,20 @@ from pysqlite2 import dbapi2 as sqlite3
 
 def storeData(temp, humid):
     with sqlite3.connect("./smartfandb") as conn:
-        conn.execute("insert into t_sensor_logs(temperature,humidity) values(%s,%s)", (temp,humid,))
+        conn.execute("INSERT INTO t_sensor_logs(temperature,humidity) VALUES(?,?)", (temp,humid,))
 
 def getLastTrio():
     with sqlite3.connect("./smartfandb") as conn:
-        with conn.cursor() as cur:
-            cur.execute("SELECT * FROM t_sensor_logs ORDER BY id DESC LIMIT 3")
-            return cur.fetchall()
+        conn.row_factory = sqlite3.Row
+        list = []
+        for row in conn.execute("SELECT * FROM (SELECT * FROM t_sensor_logs ORDER BY id DESC LIMIT 3) ORDER BY id ASC"):
+            list.append({'temperature':row['temperature'], 'humidity':row['humidity'], 'timestamp':row['timestamp']})
+        return list
 
 def getLogsAfterTimestamp(timestamp):
     with sqlite3.connect("./smartfandb") as conn:
-        with conn.cursor() as cur:
-            cur.execute("SELECT * FROM t_sensor_logs WHERE timestamp > %s ORDER BY id", (timestamp,))
-            return cur.fetchall()
+        conn.row_factory = sqlite3.Row
+        list = []
+        for row in conn.execute("SELECT * FROM t_sensor_logs WHERE timestamp > ? ORDER BY id", (timestamp,)):
+            list.append({'temperature':row['temperature'], 'humidity':row['humidity'], 'timestamp':row['timestamp']})
+        return list
