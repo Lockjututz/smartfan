2c58415748c4685589845fdc8659c8150e89c925
Decker108 <1337decker@gmail.com>
2015-07-28 18:13:00 +0200

    tellstick_client now works, modularized and ready for use

--- tellstick_client.py
+++ tellstick_client.py
@@ -1,14 +1,14 @@
 from tellcore.telldus import TelldusCore
-from time import time
+import time
 
 def activateFan():
     core = TelldusCore()
-    #lamp = core.add_device("lamp", "arctech", "selflearning-switch", house=12345, unit=1)
-    #lamp.turn_on()
-    
     for device in core.devices():
-        #print device.name
         if "Fan control device" == device.name:
             device.turn_on()
-            time.sleep(3)
+
+def deactivateFan():
+    core = TelldusCore()
+    for device in core.devices():
+        if "Fan control device" == device.name:
             device.turn_off()
