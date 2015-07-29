68f1ff1a8795a411aa9e4ce276feb14996fcccb3
Decker108 <1337decker@gmail.com>
2015-07-29 16:26:13 +0200

    Changed sqlite table to use tz aware default datetime values on inserts

--- initial_db_setup.sh
+++ initial_db_setup.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-sqlite3 -echo -bail smartfandb 'CREATE TABLE t_sensor_logs(id INTEGER PRIMARY KEY NOT NULL, temperature INTEGER NOT NULL, humidity INTEGER NOT NULL, timestamp TEXT DEFAULT CURRENT_TIMESTAMP);'
+sqlite3 -echo -bail smartfandb "CREATE TABLE t_sensor_logs(id INTEGER PRIMARY KEY NOT NULL, temperature INTEGER NOT NULL, humidity INTEGER NOT NULL, timestamp TEXT DEFAULT (datetime('now','localtime')));"
