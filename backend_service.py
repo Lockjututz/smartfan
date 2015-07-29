d80e64ff774d28b2f29784a684f57e23965b31f2
Decker108 <1337decker@gmail.com>
2015-07-29 15:44:39 +0200

    Improved error handling, added logging

--- backend_service.py
+++ backend_service.py
@@ -8,10 +8,14 @@ remote_url = "http://192.168.0.200:8071"
 def sendLogsToServer():
     req1 = requests.get(remote_url + "/latestlog/")
     if req1.status_code == 200:
-        timestamp = json.loads(req1.content)['timestamp']
+        if req1.content is None or req1.content == 'null':
+            timestamp = 0
+        else:
+            timestamp = json.loads(req1.content)['timestamp']
         payload = repo.getLogsAfterTimestamp(timestamp)
         postReqHeaders = {"Content-Type":"application/json"}
         req2 = requests.post(remote_url + "/storelogs/", data=json.dumps(payload), headers=postReqHeaders)
         if req2.status_code != 200:
-            print "Non-OK response"
-            #TODO add proper logging
+            logging.error("Non-OK storelogs response from backend server")
+    else:
+        logging.error("Non-OK latestlog response from backend server")
--- smartfan.py
+++ smartfan.py
@@ -5,6 +5,12 @@ import backend_service as backend
 import tellstick_client
 from multiprocessing import Pool
 import time
+import logging
+from sys import exc_info
+
+def setupLogging():
+    logging.basicConfig(filename='status.log',level=logging.WARNING)
+    logging.warning('Logging (re)started at %s', time.strftime("%d-%m-%Y %H:%M:%S"))
 
 def sensorReadFailure(logs):
     return logs == -1 or logs == -2 or logs == -3
@@ -14,30 +20,39 @@ def last3LogsAreOverTempLimit(last3Logs):
 
 def isDaytime(hour):
     return hour > 9 and hour < 22
+    
+def sendLogsToServer():
+    logging.info("Sending logs to server")
+    backend.sendLogsToServer()
 
 def main():
+    setupLogging()
+
     pool = Pool(processes=1)
     while (True):
-        for i in range(5):
-            logs = dht11_sensor.readSensor()
-            if sensorReadFailure(logs):
-                time.sleep(2)
-                continue
+        try:
+            for i in range(5):
+                logs = dht11_sensor.readSensor()
+                if sensorReadFailure(logs):
+                    time.sleep(2)
+                    continue
         
-        if not sensorReadFailure(logs):
-            # store sensor logs locally
-            repo.storeData(logs['temp'], logs['humid'])
+            if not sensorReadFailure(logs):
+                # store sensor logs locally
+                repo.storeData(logs['temp'], logs['humid'])
             
-            # send logs to backend server
-            pool.apply_async(backend.sendLogsToServer, [])
+                # send logs to backend server
+                pool.apply_async(sendLogsToServer, [])
+                
+                # make fan activation decision
+                if isDaytime(int(time.strftime('%H'))) and last3LogsAreOverTempLimit(repo.getLastTrio()):
+                    tellstick_client.activateFan()
+                else:
+                    tellstick_client.deactivateFan()
             
-            # make fan activation decision
-            if isDaytime(int(time.strftime('%H'))) and last3LogsAreOverTempLimit(repo.getLastTrio()):
-                tellstick_client.activateFan()
-            else:
-                tellstick_client.deactivateFan()
-        
-        time.sleep(60)
+            time.sleep(60)
+        except:
+            logging.error("%s %s",time.strftime("%d-%m-%Y %H:%M:%S"),exc_info())
 
 if __name__ == "__main__":
     main()
